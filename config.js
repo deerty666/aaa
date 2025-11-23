f4b1fed9fb83279cdc2c728ba771d8e1cefb5d0b
deerty666 <alraiee318@gmail.com>
2025-11-23 20:28:30 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+// File: config.js
+
+/* ====== بيانات الفروع - تم فصلها من Script.js ====== */
+export const BRANCH_CONFIG = {
+    'branch1': { 
+        whatsapp: '966536803598',
+        name: 'لبن الاحمدية',
+        deliveryFee: 5,
+    },
+    'branch2': {
+        whatsapp: '9665XXXXXXXX2', 
+        name: 'شمال الرياض مخرج ٦', 
+        deliveryFee: 5, 
+    },
+    'branch3': {
+        whatsapp: '9665XXXXXXXX3', 
+        name: 'الروضه خالد بن الوليد ', 
+        deliveryFee: 5,
+    }
+};
